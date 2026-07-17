7139df71c8c06d10e6cc24aeab17d2423feec7fa
Alireza Esmaeili <alirezaesmaeili.cs@gmail.com>
2026-07-17 17:49:52 +0330

    h index  fixed

--- h-index.js
+++ h-index.js
@@ -8,8 +8,8 @@ citations.sort((a, b) => b - a);
 var hIndex = function(citations) {
     for (let i = 0; i < citations.length; i++) {
         if (citations[i] >= citations.length - i) {
-            return citations.length - i;
+            return i;
         }
     }
-    return;
+    return citations.length;
 };
